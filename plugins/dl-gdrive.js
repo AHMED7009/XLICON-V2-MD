d1f2fee9b4bfbc4993ab0f3c8837a43c9c338229
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-04-27 00:57:06 +0300

    Update dl-gdrive.js

--- plugins/dl-gdrive.js
+++ plugins/dl-gdrive.js
@@ -2,26 +2,24 @@
 import fg from 'api-dylux' 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-	if (!args[0]) throw `✳️ Enter a Google Drive link`
+	if (!args[0]) throw `✳️ ارسل رابط للتحميل من قوقل درايف`
 	m.react(rwait) 
 	try {
 	let res = await fg.GDriveDl(args[0])
 	 await m.reply(`
-≡ *Google Drive DL*
+☺️ *جاري الارسال*
 
-▢ *Number:* ${res.fileName}
-▢ *Size:* ${res.fileSize}
-▢ *type:* ${res.mimetype}`)
+📥 *حجم التطبيق:* ${res.fileSize}`)
 		
 	conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
 	m.react(done)
    } catch {
-	m.reply('Error: Check the link or try another link') 
+	m.reply('*خطاء في الرابط او ان الملف تم حذفه*') 
   }
 }
 handler.help = ['gdrive']
 handler.tags = ['downloader', 'premium']
-handler.command = ['gdrive']
+handler.command = ['gdrive', 'درايف']
 handler.credit = true
 handler.premium = true
 
